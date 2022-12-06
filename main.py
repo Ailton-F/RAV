4523b96be18ca77cbfb888e5c1715ad63a429bf2
Ailton Filho <ailtonxdz@gmail.com>
2022-12-06 20:41:50 -0300

    mudanças de merge

--- main.py
+++ main.py
@@ -6,19 +6,21 @@ from models import Usuario
 from models import Doacao
 
 app = Flask(__name__)
+
 app.secret_key =  'analin'
 for bluePrint in routes.__all__:
   app.register_blueprint(bluePrint)
 
+conexao = "sqlite:///meubanco.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = conexao
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db.init_app(app)
 migrate = Migrate(app, db)
-conexao = "sqlite:///meubanco.db"
-app.config['SQLALCHEMY_DATABASE_URI'] = conexao
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-db.init_app(app)
 
 @app.route('/')
 def index():
     return render_template('home.html')
 
-app.run(host='0.0.0.0', port=81)
+if __name__ == "__main__": 
+  app.run(host='0.0.0.0', port=81, debug=True)
